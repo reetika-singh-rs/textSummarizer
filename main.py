34241324808539fc22d470bd47272ebf0703962c
singh reetika <reetikasinghchauhan0@gmail.com>
2024-07-13 20:25:57 +0530

    Add .gitattributes to handle line endings

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import sys
-import os
+from textSummarizer.logging._init_ import logger
 
-# Add the parent directory of the current file to the sys.path
-current_dir = os.path.dirname(os.path.realpath(__file__))
-parent_dir = os.path.dirname(current_dir)
-sys.path.append(parent_dir)
 
-from textSummarizer.logging_init import logger
+logger.info("Welcome to our logging")
 
-# Example usage of logger
-logger.info("Script started")
-# Add your main script logic here
-logger.info("Script finished")
+
