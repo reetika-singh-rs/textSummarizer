6aa5f25943c4c972feea0b110b47324381e4f9f7
singh reetika <reetikasinghchauhan0@gmail.com>
2024-07-17 16:21:47 +0530

    git add . git push origin main

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-"""
-Main module for text summarization.
-
-This module initializes logging and starts the text summarization process.
-"""
-
 from textSummarizer.logging.__init__ import logger
 
 logger.info("Welcome to our custom logging")
